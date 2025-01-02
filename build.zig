fae30d2ec49cdb64e53493a2ce615726d356558b
elpekenin <elpekenin@elpekenin.dev>
2025-01-02 23:37:34 +0100

    support optimization levels

--- build.zig
+++ build.zig
@@ -2,9 +2,12 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
     const ushell = b.addModule("ushell", .{
         .root_source_file = b.path("src/ushell.zig"),
+        .optimize = optimize,
+        .target =  target,
     });
 
     // Test suite
